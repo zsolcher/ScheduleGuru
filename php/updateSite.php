cc7fd28992648df35e3e63ba95712d6862b60c00
Carbohydron <colson1@trinity.edu>
2015-03-20 18:33:12 -0500

    change to updateSite, it is now synchronous

--- php/updateSite.php
+++ php/updateSite.php
@@ -2,34 +2,10 @@
 //0 IF NO NEW DATA WAS ENTERED OR IF WRONG DATA WAS ENTERED
 //1 FOR SUCCESS
 // ANY OTHER NUMBER SOMETHING WENT WRONG
-function updateAccountSettings(userID,Major,Year,FirstName,LastName){
-	var res = {};	
-	if(userID === "" || Major === "" || Year === "" || FirstName === "" || LastName === ""){
-			
-	}else{
-		var xmlhttp;
-		if (window.XMLHttpRequest){
-  			xmlhttp=new XMLHttpRequest();
-  		}else{
-  			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  		}
-  		xmlhttp.onreadystatechange=function(){
-  			if(xmlhttp.readyState==4 && xmlhttp.status==200){
-  				res = JSON.parse(xmlhttp.responseText);
-  				
-  			}
-  		}
-  		xmlhttp.open("GET","queryServer.php?request=updateAccount&userID="+userID+"&Major="+Major+"&Year="+Year+"&FirstName="+FirstName+"&LastName="+LastName,true);
-  		xmlhttp.send();
-  		return res;	
-	}
-	
-}
-//TODO FORMAT RETURN DATA INTO AN OBJECT
-//OBJECT RETURNED CONTAINS USER DATA: FIRSTNAME, LASTNAME, MAJOR, YEAR, EMAIL, PASSWORD
+var result = {};
+
 function sendQuery(data){
 	var xmlhttp;
-	var res = {};
 	if (window.XMLHttpRequest){
   		xmlhttp=new XMLHttpRequest();
   	}else{
@@ -37,115 +13,35 @@ function sendQuery(data){
   	}
   	xmlhttp.onreadystatechange=function(){
   		if(xmlhttp.readyState==4 && xmlhttp.status==200){
-  			res = JSON.parse(xmlhttp.responseText);
-  			
+  			result = JSON.parse(xmlhttp.responseText);
   		}
   	}
-  	xmlhttp.open("GET","queryServer.php?"+data,true);
+  	xmlhttp.open("GET","queryServer.php?"+data,false);
   	xmlhttp.send();
-  	return res;
+}
+function updateAccountSettings(userID,Major,Year,FirstName,LastName){
+	if(userID === "" || Major === "" || Year === "" || FirstName === "" || LastName === ""){
+	}else{
+  		sendQuery("request=updateAccount&UserID="+userID+"&Major="+Major+"&Year="+Year+"&FirstName="+FirstName+"&LastName="+LastName);
+	}
 }
 function getUserData(userID){
-	var xmlhttp;
-	var res = {};
-	if (window.XMLHttpRequest){
-		xmlhttp=new XMLHttpRequest();
- 	}else{
- 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
- 	}
-  	xmlhttp.onreadystatechange=function(){
-  		if(xmlhttp.readyState==4 && xmlhttp.status==200){
-  			res = JSON.parse(xmlhttp.responseText);
-  		}
-  	}
-  	xmlhttp.open("GET","queryServer.php?request=userData&UserID="+userID,true);
-  	xmlhttp.send();
-	return res;
+  	sendQuery("request=userData&UserID="+userID);
 }
 function getPrereqs(classID){
-	var xmlhttp;
-	var res = {};
-	if (window.XMLHttpRequest){
-		xmlhttp=new XMLHttpRequest();
- 	}else{
- 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
- 	}
-  	xmlhttp.onreadystatechange=function(){
-  		if(xmlhttp.readyState==4 && xmlhttp.status==200){
-  			res = JSON.parse(xmlhttp.responseText);
-  		}
-  	}
-  	xmlhttp.open("GET","queryServer.php?request=prereqs&ClassID="+classID,true);
-  	xmlhttp.send();
-  	return res;
+  	sendQuery("request=prereqs&ClassID="+classID);
 }
 function getClassData(classID){
-	var xmlhttp;
-	var res = {};
-	if (window.XMLHttpRequest){
-		xmlhttp=new XMLHttpRequest();
- 	}else{
- 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
- 	}
-  	xmlhttp.onreadystatechange=function(){
-  		if(xmlhttp.readyState==4 && xmlhttp.status==200){
-  			res = JSON.parse(xmlhttp.responseText);
-  		}
-  	}
-  	xmlhttp.open("GET","queryServer.php?request=classData&ClassID="+classID,true);
-  	xmlhttp.send();
-  	return res;
+  	sendQuery("request=classData&ClassID="+classID);
 }
 function classSameAs(classID){
-	var xmlhttp;
-	var res = {};
-	if (window.XMLHttpRequest){
-		xmlhttp=new XMLHttpRequest();
- 	}else{
- 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
- 	}
-  	xmlhttp.onreadystatechange=function(){
-  		if(xmlhttp.readyState==4 && xmlhttp.status==200){
-  			res = JSON.parse(xmlhttp.responseText);
-  		}
-  	}
-  	xmlhttp.open("GET","queryServer.php?request=sameAs&ClassID="+classID,true);
-  	xmlhttp.send();
-  	return res;	
+  	sendQuery("request=sameAs&ClassID="+classID);
 }
 function classesTaken(userID){
-	var xmlhttp;
-	var res = {};
-	if (window.XMLHttpRequest){
-		xmlhttp=new XMLHttpRequest();
- 	}else{
- 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
- 	}
-  	xmlhttp.onreadystatechange=function(){
-  		if(xmlhttp.readyState==4 && xmlhttp.status==200){
-  			res = JSON.parse(xmlhttp.responseText);
-  		}
-  	}
-  	xmlhttp.open("GET","queryServer.php?request=classesTaken&UserID="+userID,true);
-  	xmlhttp.send();
-  	return res;
+  	sendQuery("request=classesTaken&UserID="+userID);
 }
 
 function login(username,password){
-	var xmlhttp;
-	var res = {};
-	if (window.XMLHttpRequest){
-		xmlhttp=new XMLHttpRequest();
- 	}else{
- 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
- 	}
-  	xmlhttp.onreadystatechange=function(){
-  		if(xmlhttp.readyState==4 && xmlhttp.status==200){
-  			res = JSON.parse(xmlhttp.responseText);
-  		}
-  	}
-  	xmlhttp.open("GET","queryServer.php?request=login&username="+username+"&password="+password,false);
-  	xmlhttp.send();
-  	return res;
+  	sendQuery("request=login&username="+username+"&password="+password);
 }
 </script>
