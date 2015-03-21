fe60afad1d293cc8a65ba8fa77b8a92b762892d4
rbierman <rbierman@trinity.edu>
2015-03-21 15:20:13 -0500

    Working towards using Kendricks parser

---
+++ buildJDBCPath.sh
@@ -0,0 +1,4 @@
+echo $CLASSPATH;
+CLASSPATH=`pwd`"/mysql-connector-java-5.1.34/mysql-connector-java-5.1.34-bin.jar:"$CLASSPATH;
+export CLASSPATH;
+echo $CLASSPATH;
---
+++ clean_javac_output.sh
@@ -0,0 +1,4 @@
+rm MainDAO.class;
+rm SchoolClass.class
+rm Scheduler.class
+rm ImportTranscript.class
