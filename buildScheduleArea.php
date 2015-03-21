db50de2beeaf5cfe4e80385c0405fbad3fb7d55e
Zach Solcher <zsolcher@gmail.com>
2015-03-20 19:44:21 -0500

    Abstracted the page areas from the index.php into separate files.

---
+++ buildScheduleArea.php
@@ -0,0 +1,31 @@
+<html>
+	<core-pages class="fancy" selected="0">
+            <div class="page" id="pageUploadTranscript">
+                <paper-fab class="fabNavLeft" icon="chevron-left"></paper-fab>
+                one
+                <paper-fab class="fabNavRight" icon="chevron-right"> </paper-fab>
+            </div>
+            <div class="page" id="page2">
+                <paper-fab class="fabNavLeft" icon="chevron-left"></paper-fab>
+                two
+                <paper-fab class="fabNavRight" icon="chevron-right"> </paper-fab>
+            </div>
+            <div class="page" id="page3">
+                <paper-fab class="fabNavLeft" icon="chevron-left"></paper-fab>
+                three
+                <paper-fab class="fabNavRight" icon="chevron-right"> </paper-fab>
+            </div>
+            <div class="page" id="page4">
+                <paper-fab class="fabNavLeft" icon="chevron-left"></paper-fab>
+                four
+                <paper-fab class="fabNavRight" icon="chevron-right"> </paper-fab>
+            </div>
+            <div class="page" id="page5">
+                <paper-fab class="fabNavLeft" icon="chevron-left"></paper-fab>
+                five
+                <paper-fab class="fabNavRight" icon="chevron-right"> </paper-fab>
+            </div>
+	</core-pages>
+	
+        <script src="js/build_schedule_area.js"></script>
+</html>
