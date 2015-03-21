8579bd1f5385281b62d1a1c27d97d56359616c71
Lu <lliu@trinity.edu>
2015-03-21 17:06:59 -0500

    modified homeArea.php, letter style

--- homeArea.php
+++ homeArea.php
@@ -10,13 +10,17 @@ h1 {
 } 
 </style>
 
-
+<body>
         <center>
-			<h1>Welcome to ScheduleGuru!</h1>
+			<h1>A Letter to the User</h1>
         </center>
         <hr>
         <p>
-            Here's a brief overview on how to use the ScheduleGuru App.
+            <h2>
+				Dear User,
+				<br>
+				<br>
+				Here's a brief overview on how to use the ScheduleGuru App.
             If you have not already signed up for an account, please do so now.
             If you are already a registered user, please proceed to login.
             Once you have logged in, you will be greeted by a homepage, in which
@@ -25,7 +29,18 @@ h1 {
             Use the left "<" and right ">" buttons to navigate through the different stages.
             You may also click on any point of the progress bar to go back to a previous section.
             Have fun scheduling!
-<br>
+				<br>
+				<br>
+				Best,
+				
+				<br>
+				<br>
+				ScheduleGuru Team
+				<br>
+				<br>
             P.S. Please email us with any feedback :)
+				
+			</h2>
         </p>
+</body>
 </html>
