a0f39da246b63068d1be8a1071b3787f22859d66
Lu <lliu@trinity.edu>
2015-03-22 01:04:53 -0500

    Last push of the day, finished all the html for the pages, will work on CSS for them tomorrow.

--- savedSchedulesArea.php
+++ savedSchedulesArea.php
@@ -10,23 +10,20 @@
 					</core-menu>
 				</paper-dropdown>
 			</paper-dropdown-menu>
-
+		
 
 			<paper-dropdown-menu label = "Delete Schedule">
 				<paper-dropdown class = "dropdown">
 					<core-menu class="menu">
-						<paper-item>Freshman</paper-item>
-						<paper-item>Sophomore</paper-item>
-						<paper-item>Junior</paper-item>
-						<paper-item>Senior</paper-item>
-						<paper-item>Super Senior</paper-item>
+						<paper-item>Fall 2014</paper-item>
+			
 					</core-menu>
 				</paper-dropdown>
 			</paper-dropdown-menu>
 						
 
 			<center>
-            <paper-button raised class="acctButton" id="btnAcct" >Change Settings</paper-button>
+            <paper-button raised class="acctButton" id="btnAcct" >Save</paper-button>
 			</center> 
         </form>
 
