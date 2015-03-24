a0014b371e0ed052c93b9c4c5504b9745a42289c
rbierman <rbierman@trinity.edu>
2015-03-24 15:02:16 -0500

    Changed layout of account settings slightly

--- accountSettingsArea.php
+++ accountSettingsArea.php
@@ -4,16 +4,18 @@
         <form id="accountSettingsForm" action="" method="post">
 			<center>
 			
+			<h3 style="color:white"> First Name:
 			<paper-input-decorator class="textInput" id="inputFname" name="acct_fname" label="First Name" floatingLabel>
                 <input is="core-input"id="txtFname" value="">
             </paper-input-decorator>
-			
-			<br>
-			<br>
-            <paper-input-decorator class="textInput" id="inputLname" name="acct_lname" label="Last Name"  floatingLabel>
+			</h3>
+
+			<h3 style="color:white"> Last Name: 
+			<paper-input-decorator class="textInput" id="inputLname" name="acct_lname" label="Last Name"  floatingLabel>
                 <input is="core-input" id="txtLname" value="">
             </paper-input-decorator>
-			
+			</h3>
+
 			<br>
 			<br>
 			<!--
@@ -37,14 +39,12 @@
 					</core-menu>
 				</paper-dropdown>
 			</paper-dropdown-menu>
-
-			<br>
 			<!--
             <paper-input-decorator class="textGrade" id="inputGrade" name="acct_grade" label="Grade"  floatingLabel>
                 <input is="core-input" id="txtGrade" value="">
             </paper-input-decorator>
 			-->
-			
+
 			<paper-dropdown-menu label = "Grade">
 				<paper-dropdown class = "dropdown">
 					<core-menu class="menu">
@@ -56,23 +56,21 @@
 					</core-menu>
 				</paper-dropdown>
 			</paper-dropdown-menu>
-						
 			<br>
-			
+
+			<h3 style="color:white"> Email:
             <paper-input-decorator class="textInput" id="inputEmail" name="login_email" label="email address" floatingLabel>
                 <input is="core-input"id="txtEmail" value="">
             </paper-input-decorator>
+			</h3>
 
-			<br>
-			<br>
-			
+			<h3 style="color:white"> Pswrd:
             <paper-input-decorator class="textInput" id="inputPassword" name="login_password" label="password"  floatingLabel>
                 <input is="core-input" id="txtPassword" type="password" value="">
             </paper-input-decorator>
 				</center>
-			<br>
-            <paper-button raised class="acctButton" id="btnAcct" >Change Settings</paper-button>
-			
+			</h3>
+            <paper-button raised class="acctButton" id="btnAcct" >Update</paper-button>
         </form>
 
 </html>
